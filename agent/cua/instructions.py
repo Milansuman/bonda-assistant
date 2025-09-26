284bb2b6147fd12a32035ca5a0eb2d4770d783f9
Aravind Manoj <aravindmng47@gmail.com>
2025-09-26 16:44:24 +0530

    fix cua

--- agent/cua/instructions.py
+++ agent/cua/instructions.py
@@ -3,8 +3,20 @@ from tools import TOOLS
 
 rendered_tools = render_text_description(TOOLS)
 
-SYSTEM_PROMPT = f"""You are Bonda, an AI assistant that can automate tasks on the computer.
+SYSTEM_PROMPT = f"""\
+You are BondaAI, an autonomous AI agent that can control the computer using different tools. You have access to the following set of tools. 
+Here are the names and descriptions for each tool:
 
-These are your tools:
 {rendered_tools}
+
+Given the user input, return the name and input of the tool to use. 
+Return your response as a JSON blob with 'name' and 'arguments' keys.
+
+The `arguments` should be a dictionary, with keys corresponding 
+to the argument names and the values corresponding to the requested values.
+
+When you are done, call the `finish` tool to mark the task as finished.
+If you encounter an error, call the `error` tool to mark the task as failed.
 """
+
+print(SYSTEM_PROMPT)
