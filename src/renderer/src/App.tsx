5eb3bec38eee899a2030d902c8300e475f89fbb3
Abin Thomas <followgamer102@gmail.com>
2025-09-26 17:22:01 +0530

    fix: minor fix in ui and added askBonda function

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,10 +3,28 @@ import { useRef, useState } from "react";
 
 export default function App() {
   const [response, setResponse] = useState("");
+  const [prompt, setPrompt] = useState<string>("");
+  const [loading,setLoading] = useState(false);
   const promptInputRef = useRef<HTMLInputElement | null>(null);
+
+  const askBonda = async (prompt: string) => {
+    try{
+      setLoading(true);
+      setResponse("Thinking...");
+      //const res = await functionCall( prompt);
+      //setResponse(res.response);
+    }catch(err){
+      setResponse("Error occurred. Please try again.");
+      console.error(err);
+    }finally{
+      setLoading(false);
+    }
+  }
   return (
     <>
-      <div className="bonda-overlay"></div>
+      {loading && (
+        <div className="bonda-overlay"></div>
+      )}
       <div className="w-screen h-screen flex items-center justify-center bg-transparent">
         <div className="w-[600px] bg-[#0c0e10] border border-white/10 rounded-2xl shadow-xl backdrop-blur-lg text-gray-200">
           <div className="flex items-center gap-3 px-4 py-3 border-b border-white/10">
@@ -17,9 +35,12 @@ export default function App() {
               ref={promptInputRef}
               placeholder="Ask me anything!"
               className="flex-1 bg-transparent outline-none text-sm placeholder-gray-400"
+              onChange={(e) => setPrompt(e.currentTarget.value)}
               onKeyDown={(event) => {
                 if(event.key === "Enter"){
                   const prompt = event.currentTarget.value;
+                  setPrompt(prompt);
+                  askBonda(prompt);
                   if(promptInputRef.current) promptInputRef.current.value = "";
 
                   
@@ -27,10 +48,10 @@ export default function App() {
               }}
             />
             <Mic color="#9ca3af" size={18} className="cursor-pointer hover:text-white" />
-            <Play color="#9ca3af" size={18} className="cursor-pointer hover:text-white" />
+            <Play color="#9ca3af" size={18} onClick={() => askBonda(prompt)} className="cursor-pointer hover:text-white" />
           </div>
           <div className="flex flex-col p-5 min-h-20 h-fit max-h-96 overflow-auto">
-            
+            {response}
           </div>
         </div>
       </div>
