7f29a9169c912d9680ce1a26bfd2d155806f7c20
Aravind Manoj <aravindmng47@gmail.com>
2025-09-26 16:43:10 +0530

    fix: tools

--- agent/cua/main.py
+++ agent/cua/main.py
@@ -1,11 +1,13 @@
 import os
+import json
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from instructions import SYSTEM_PROMPT
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_core.output_parsers import JsonOutputParser
+from tools import invoke_tool
 
 load_dotenv()
 
@@ -20,10 +22,16 @@ def root():
 
 @app.post("/run")
 def run(request: RunRequest):
+    parser = JsonOutputParser()
+
     model = ChatOpenAI(
-        model="bytedance/ui-tars-1.5-7b",
+        model="qwen/qwen2.5-vl-72b-instruct",
+        # model="bytedance/ui-tars-1.5-7b",
         api_key=os.environ["OPENROUTER_API_KEY"],
         base_url="https://openrouter.ai/api/v1",
+        extra_body={
+            "provider": {"only": ["hyperbolic"]}
+        }
     )
     
     messages = [SystemMessage(content=SYSTEM_PROMPT)]
@@ -36,9 +44,30 @@ def run(request: RunRequest):
         ]
     ))
     
-    chain = messages | model | JsonOutputParser()
+    finish = False
+    function_exec_state = False
     try:
-        result = chain.run()
-        return {"result": result}
+        while (not finish or function_exec_state):
+            response = model.invoke(messages)
+            llm_result = parser.invoke(response)
+            print("Tool: ", llm_result)
+            function_exec_state = False
+            if (not llm_result):
+                break
+            if (llm_result["name"] == "finish" or llm_result["name"] == "error"):
+                finish = True
+            else:
+                function_exec_state = True
+                messages.append(AIMessage(
+                    content=[
+                        {
+                            "type": "text",
+                            "text": json.dumps(llm_result)
+                        }
+                    ]
+                ))
+                tool_result = invoke_tool(llm_result)
+                if (tool_result):
+                    messages.append(HumanMessage(content=[tool_result]))
     except Exception as e:
         return {"error": str(e)}
--- agent/cua/tools.py
+++ agent/cua/tools.py
@@ -1,12 +1,202 @@
+import time
+import pyautogui
+import base64
+from io import BytesIO
 from langchain_core.tools import tool
+from enum import Enum
+from typing import Any, Dict, Optional, TypedDict
+from langchain_core.runnables import RunnableConfig
+
+ACTION_DELAY = 0.1
+
+def move_mouse_to(x: int, y: int):
+    pyautogui.moveTo(x, y)
+    time.sleep(ACTION_DELAY)
+
+class KEYS(str, Enum):
+    Enter = "Enter"
+    Backspace = "Backspace"
+    Delete = "Delete"
+    Tab = "Tab"
+    Escape = "Escape"
+    Space = "Space"
+    ArrowUp = "ArrowUp"
+    ArrowDown = "ArrowDown"
+    ArrowLeft = "ArrowLeft"
+    ArrowRight = "ArrowRight"
+    PageUp = "PageUp"
+    PageDown = "PageDown"
+
+@tool
+def fetchScreen():
+    """Get the current page screenshot.
+    
+    """
+    screenshot = pyautogui.screenshot()
+    buffer = BytesIO()
+    screenshot.save(buffer, format="PNG")
+    img_bytes = buffer.getvalue()
+    img_base64 = base64.b64encode(img_bytes).decode('utf-8')
+    print("Screen fetched")
+    return { "type:": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{img_base64}" } }
 
 @tool
 def click(x: int, y: int):
-    """Click tool"""
+    """Simulate a click on the specified x and y coordinates.
+    
+    Args:
+        x: The x coordinate of the click point.
+        y: The y coordinate of the click point.
+    
+    """
+    move_mouse_to(x, y)
+    pyautogui.click()
+    time.sleep(ACTION_DELAY)
+    return { "type": "text", "text": "Clicked successfully" }
 
 @tool
-def typeText(text: str):
-    """Type text tool"""
+def typeText(x: int, y: int, text: str):
+    """Type text into the specified field. Located by its x and y coordinates.
+    
+    Args:
+        x: The x coordinate of the click point.
+        y: The y coordinate of the click point.
+        text: The text to type.
+    
+    """
+    move_mouse_to(x, y)
+    pyautogui.click()
+    time.sleep(ACTION_DELAY)
+    pyautogui.typewrite(text, interval=0.05)
+    time.sleep(ACTION_DELAY)
+    return { "type": "text", "text": "Text typed successfully" }
+
+@tool
+def clearValue(x: int, y: int):
+    """Clears the value in the specified input field. Located by its x and y coordinates.
+    
+    Args:
+        x: The x coordinate of the click point.
+        y: The y coordinate of the click point.
+    
+    """
+    move_mouse_to(x, y)
+    pyautogui.click()
+    time.sleep(ACTION_DELAY)
+    pyautogui.hotkey('ctrl', 'a')
+    time.sleep(ACTION_DELAY)
+    pyautogui.press('backspace')
+    time.sleep(ACTION_DELAY)
+    return { "type": "text", "text": "Value cleared successfully" }
+
+@tool
+def keyPress(key: KEYS):
+    """Send key press to the page.
+    
+    Args:
+        key: The key to press.
+    
+    """
+    pyautogui.press(key)
+    time.sleep(ACTION_DELAY)
+    return { "type": "text", "text": "Key pressed successfully" }
+
+@tool
+def scroll(x: int, y: int, xDistance: int, yDistance: int):
+    """Scrolls the portion of the page specified by the x and y coordinates.
+    
+    Args:
+        x: The x coordinate of the portion to scroll.
+        y: The y coordinate of the portion to scroll.
+        xDistance: The distance to scroll along the X axis (pixels).
+        yDistance: The distance to scroll along the Y axis (pixels).
+    
+    """
+    move_mouse_to(x, y)
+    # pyautogui.scroll(amount_to_scroll, x=None, y=None)
+    # This scrolls vertically. For horizontal scroll, pyautogui.hscroll() is used.
+    if yDistance != 0:
+        pyautogui.scroll(yDistance)  # Positive yDistance scrolls up, negative scrolls down
+        time.sleep(ACTION_DELAY)
+    if xDistance != 0:
+        pyautogui.hscroll(xDistance)  # Positive xDistance scrolls right, negative scrolls left
+        time.sleep(ACTION_DELAY)
+    return { "type": "text", "text": "Scrolled successfully" }
+
+@tool
+def wait(ms: int):
+    """Wait for a specified amount of time.
+    
+    Args:
+        ms: The amount of time to wait in milliseconds.
+    
+    """
+    time.sleep(ms / 1000)
+    return { "type": "text", "text": "Waited successfully" }
+
+@tool
+def finish(message: str):
+    """Mark the task as finished. No further actions will be taken.
+    
+    Args:
+        message: The message to print.
+    
+    """
+
+@tool
+def error(message: str):
+    """Mark the task as failed. No further actions will be taken.
+    
+    Args:
+        message: The message to print.
+    
+    """
+
+@tool
+def message(text: str):
+    """Print a message.
+    
+    Args:
+        text: The message to print.
+    
+    """
+    return { "type": "text", "text": "Message printed successfully" }
+
+TOOLS = [
+    fetchScreen,
+    click,
+    typeText,
+    clearValue,
+    keyPress,
+    scroll,
+    wait,
+    finish,
+    error,
+    message
+]
+
+class ToolCallRequest(TypedDict):
+    """A typed dict that shows the inputs into the invoke_tool function."""
+    name: str
+    arguments: Dict[str, Any]
 
 
-tools = [click, typeText]
+def invoke_tool(
+    tool_call_request: ToolCallRequest, config: Optional[RunnableConfig] = None
+):
+    """A function that we can use the perform a tool invocation.
+
+    Args:
+        tool_call_request: a dict that contains the keys name and arguments.
+            The name must match the name of a tool that exists.
+            The arguments are the arguments to that tool.
+        config: This is configuration information that LangChain uses that contains
+            things like callbacks, metadata, etc.See LCEL documentation about RunnableConfig.
+
+    Returns:
+        output from the requested tool
+    """
+    tool_name_to_tool = {tool.name: tool for tool in TOOLS}
+    name = tool_call_request["name"]
+    requested_tool = tool_name_to_tool[name]
+    return requested_tool.invoke(tool_call_request["arguments"], config=config)
